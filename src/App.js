55c8be8f364b452ab72f564491d8eec8a5343b3d
Lucy Veron <lucymveron@gmail.com>
2023-02-18 16:37:28 +0100

    File structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.scss';
+import IsoMenu from './examples/iso-menu'
 
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li style={{ "--i": 6 }}><a href="#">Home</a></li>
-        <li style={{ "--i": 5 }}><a href="#">About</a></li>
-        <li style={{ "--i": 4 }}><a href="#">Services</a></li>
-        <li style={{ "--i": 3 }}><a href="#">Portfolio</a></li>
-        <li style={{ "--i": 2 }}><a href="#">Our Team</a></li>
-        <li style={{ "--i": 1 }}><a href="#">Contact</a></li>
-      </ul>
+      <IsoMenu />
     </div>
   );
 }
