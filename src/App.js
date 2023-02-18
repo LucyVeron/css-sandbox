ec0674f936e51534e9c319c1632b60fbf5d45b2d
Lucy Veron <lucymveron@gmail.com>
2023-02-18 17:08:27 +0100

    Grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.scss';
+import Glassmorphism from './examples/glassmorphism';
 import IsoMenu from './examples/iso-menu'
 
 function App() {
   return (
     <div className="App">
-      <IsoMenu />
+      <div>
+        <IsoMenu />
+      </div>
+      <div>
+        <Glassmorphism />
+      </div>
     </div>
   );
 }
